508eb8e8c4c146f3b4f866ff1c22c6355b820222
Jhonatan <larico1599@gmail.com>
2022-06-16 21:02:35 -0500

    d

---
+++ Dia_1/01-variables.py
@@ -0,0 +1,20 @@
+nombre = "jhon"
+edad = 29
+
+despedida = '''dddddddddddddd
+ddddddddddddd
+ddddddddddddd
+ddddddddddddd'''
+
+## variable indefinida
+d = None
+d = 29
+
+mascota1, mascota2 = "m","l"
+
+print(type(d))
+
+
+
+
+
